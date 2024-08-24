d0505afe5c4bf307aa7fa09cbde74a578ebb5e6c
Michael Neumann <mneumann@ntecs.de>
2024-08-24 02:51:48 +0200

    Keep a changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# ChangeLog
+
+## 0.1.0-rc.0 (24.08.2024)
+
+* Initial release
--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule ScenicFromSvg.MixProject do
 
   defp package() do
     [
-      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      files: ["lib", "mix.exs", "README.md", "CHANGELOG.md", "LICENSE"],
       maintainers: ["Michael Neumann"],
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/mneumann/scenic_from_svg"}
