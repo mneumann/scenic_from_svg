27af30cbcc088a2929d4a79300d8dff434ac1a4c
Michael Neumann <mneumann@ntecs.de>
2024-08-24 02:47:19 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ScenicFromSvg.MixProject do
   def project do
     [
       app: :scenic_from_svg,
-      version: "0.0.1",
+      version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       # build_embedded: true,
